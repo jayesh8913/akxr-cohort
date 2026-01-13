3f2d0210c78a491a08025a42fa68b50e2d706c23
jayesh8913 <jayeshpatil030405@gmail.com>
2026-01-13 15:47:49 +0530

    practiced a bit of fs lib

---
+++ practice/fe02-w2/file.js
@@ -0,0 +1,22 @@
+const { log } = require("console");
+const fs= require("fs");
+//console.log(fs);
+
+fs.writeFileSync('./test.txt','wassup mah boi');// used to make and enter data(replace the new data w the prev one )
+fs.writeFileSync('./test.txt','yoyo');
+
+//async
+fs.writeFile('./test.txt',"hello world async",(err)=>{});  
+
+const result=fs.readFileSync("./test.txt","utf-8");
+console.log(result);
+
+fs.readFile('./test.txt','utf-8',(err,result)=>{})
+
+// in conclusion async fns doesnot result as same as sync fns do
+
+fs.appendFileSync('./test.txt',`\n`,new Date().getDate().toLocaleString);
+
+fs.cpSync('./test.txt','copy.txt');
+
+fs.unlinkSync('copy.txt')//to del 
