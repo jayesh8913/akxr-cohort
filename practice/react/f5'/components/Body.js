cc41fe5d5d65a23be286bbad0e1ec85bde567c5f
jayesh8913 <jayeshpatil030405@gmail.com>
2026-02-05 17:01:22 +0530

    added swiggy api

--- practice/react/f5'/components/Body.js
+++ practice/react/f5'/components/Body.js
@@ -1,22 +1,52 @@
-import { useState } from "../node_modules/react";
-import resList from "../utils/mockdata";
-import RestaurantCard from "./RestaurentCard";
+import { useEffect, useState } from "react";
+import RestaurentCard from "./RestaurentCard";
 
 const Body = () => {
-    const[listofres,setlistofres]= useState(resList)
+  const [listofres, setlistofres] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.6326695&lng=73.8220565&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+   const restaurants =json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+
+
+    console.log(json.data.cards);
+
+    setlistofres(restaurants || []);
+  };
+  
+
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-button" onClick={()=>{
+        <button
+          onClick={() => {
             const filteredlist = listofres.filter(
-                (res)=> res.data.avgRating>4
+              (res) => res?.info?.avgRating > 4
             );
-            setlistofres(filteredlist)
-        }}>filtered res</button>
+            setlistofres(filteredlist);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
       </div>
+
       <div className="res-container">
         {listofres.map((restaurant) => (
-          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+          <RestaurentCard
+            key={restaurant?.info?.id}
+            resData={restaurant?.info}
+          />
         ))}
       </div>
     </div>
--- practice/react/f5'/components/RestaurentCard.js
+++ practice/react/f5'/components/RestaurentCard.js
@@ -1,29 +1,28 @@
-const RestaurantCard = (props) => {
-  const { resData } = props;
+const RestaurantCard = ({ resData }) => {
+ const {
+  cloudinaryImageId,
+  name,
+  avgRating,
+  cuisines,
+  costForTwo,
+  deliveryTime
+} = resData;
 
-  const {
-    cloudinaryImageId,
-    name,
-    avgRating,
-    cuisines,
-    costForTwo,
-    deliveryTime,
-  } = resData?.data;
 
   return (
-    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+    <div className="res-card">
       <img
         className="res-logo"
         alt="res-logo"
         src={
-          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+          "https://media-assets.swiggy.com/swiggy/image/upload/" +
           cloudinaryImageId
         }
       />
       <h3>{name}</h3>
-      <h4>{cuisines.join(", ")}</h4>
+      <h4>{cuisines?.join(", ")}</h4>
       <h4>{avgRating} stars</h4>
-      <h4>₹{costForTwo / 100} FOR TWO</h4>
+      <h4>{costForTwo}</h4>
       <h4>{deliveryTime} minutes</h4>
     </div>
   );
