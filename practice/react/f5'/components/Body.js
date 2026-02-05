1918c2583110600be16a081f8b171e72afb5aca2
jayesh8913 <jayeshpatil030405@gmail.com>
2026-02-05 10:44:01 +0530

    added useState hook to body

--- practice/react/f5'/components/Body.js
+++ practice/react/f5'/components/Body.js
@@ -1,12 +1,21 @@
+import { useState } from "../node_modules/react";
 import resList from "../utils/mockdata";
 import RestaurantCard from "./RestaurentCard";
 
 const Body = () => {
+    const[listofres,setlistofres]= useState(resList)
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button className="filter-button" onClick={()=>{
+            const filteredlist = listofres.filter(
+                (res)=> res.data.avgRating>4
+            );
+            setlistofres(filteredlist)
+        }}>filtered res</button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listofres.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
